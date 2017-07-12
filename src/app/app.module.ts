f0bdcf05e293c37a7d8a4be9304b873bc186a002
vijayananth Natarajan <nikeananth@gmail.com>
2017-07-11 21:03:05 -0400

    Modified code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DemoComponentComponent } from './demo-component/demo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponentComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +15,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
